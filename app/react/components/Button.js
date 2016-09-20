21bd46dccf7d494ffbaadd18f9e6da7eac4e2353
Paweł Jędrzejczyk <pawel.jedrzejczyk@netguru.pl>
2016-09-20 12:46:08 +0200

    proptype children not required

--- app/react/components/Button.js
+++ app/react/components/Button.js
@@ -8,7 +8,7 @@ const Button = ({ children, onClick }) => (
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
 };
 
 export default Button;
