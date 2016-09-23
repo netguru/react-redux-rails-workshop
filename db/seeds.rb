292b3bd993ea7b64ef9d86efdd3d8f753b57d8fe
Piotr Pawlik <piotr.pawlik@netguru.pl>
2016-09-23 15:29:50 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+10.times do |i|
+  u = User.find_or_initialize_by(email: "user+#{i}@example.com")
+  u.update_attributes(password: 'Testing1')
+end
+
 50.times do |i|
   r = Restaurant.find_or_initialize_by(address: "Address #{i}")
   r.update_attributes(
     name: FFaker::NameIT.name,
-    description: ['burger', 'pizza', 'sushi', 'ice cream', 'pierozki'].sample
+    description: ['burger', 'pizza', 'sushi', 'ice cream', 'pierozki'].sample,
+    user: User.all.sample
   )
 
   r.comments << (0..5).to_a.map do |i|
     Comment.new(
       rating: (1..5).to_a.sample,
-      description: FFaker::Lorem.sentence
+      description: FFaker::Lorem.sentence,
+      user: User.where.not(id: r.user_id).sample
     )
   end
 end
