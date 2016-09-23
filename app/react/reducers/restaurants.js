83c670a7ae27ea434cb9331e558dd9e7247fb355
Piotr Pawlik <piotr.pawlik@netguru.pl>
2016-09-23 11:07:52 +0200

    Added sample restaurants

--- app/react/reducers/restaurants.js
+++ app/react/reducers/restaurants.js
@@ -1,4 +1,17 @@
-export default (state = [], action) => {
+const sampleRestaurants = [
+  {
+    id: 1,
+    name: 'Test #1',
+    address: 'Testowy address',
+  },
+  {
+    id: 2,
+    name: 'Test #2',
+    address: 'Testowy address #2',
+  },
+]
+
+export default (state = sampleRestaurants, action) => {
   switch (action.type) {
     default:
       return state;
