ffdfd19b6f52d227c2126691a687e67d32548518
adam-szczombrowski <adam.szczombrowski@gmail.com>
2016-09-23 14:14:25 +0200

    add validations to models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :comments
 
+  validates :name, :address, :description, presence: true
+
   def average_rating
     return unless comments.present?
     comments.inject(0.0) { |result, el| result + el.rating } / comments.size
