df3962f69795b46fab197c49512faf154b45d07a
JustynaStebel <justyna.stebel@netguru.pl>
2016-09-23 12:34:26 +0200

    Add some styling to show

--- app/react/components/RestaurantView.js
+++ app/react/components/RestaurantView.js
@@ -1,10 +1,10 @@
 import React, { PropTypes } from 'react';
 
 const RestaurantView = ({ restaurant }) => (
-  <div className="restaurant">
-    <p className="restaurant-name">{restaurant.name}</p>
-    <p className="restaurant-address">{restaurant.address}</p>
-    <p className="restaurant-description">{restaurant.description}</p>
+  <div className="panel panel-primary">
+    <p className="panel-heading">{restaurant.name}</p>
+    <p className="panel-body">{restaurant.address}</p>
+    <p className="panel-body">{restaurant.description}</p>
   </div>
 );
 
