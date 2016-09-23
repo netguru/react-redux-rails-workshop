a763d59ec9012356f587ce38f514d2e233f7bb3d
Piotr Pawlik <piotr.pawlik@netguru.pl>
2016-09-23 11:07:44 +0200

    Fix restaurant link

--- app/react/components/RestaurantListItem.js
+++ app/react/components/RestaurantListItem.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 
 const RestaurantsListItem = ({ restaurant }) => (
-  <a href='/restaurant/{restaurant.id}'>{restaurant.name}</a>
+  <a href={`/restaurants/${restaurant.id}`}>{restaurant.name}</a>
 );
 
 export default RestaurantsListItem;
-
