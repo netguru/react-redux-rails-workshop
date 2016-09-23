18796e97df595979218bec1ceb2bf95d8eda4df7
Piotr Pawlik <piotr.pawlik@netguru.pl>
2016-09-23 13:36:03 +0200

    Added description to index

--- app/react/components/RestaurantListItem.js
+++ app/react/components/RestaurantListItem.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 const RestaurantsListItem = ({ restaurant }) => (
-  <li className="list-group-item"><a href={`/restaurants/${restaurant.id}`}>{restaurant.name}</a><span className="badge pull-right">10</span></li>
+  <li className="list-group-item">
+    <a href={`/restaurants/${restaurant.id}`}>{restaurant.name}</a>
+    <span className="badge pull-right">10</span>
+    <span className="badge pull-right primary">{restaurant.description}</span>
+  </li>
 );
 
 export default RestaurantsListItem;
