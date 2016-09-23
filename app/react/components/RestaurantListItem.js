3803069d4dcc24add60f5097d516de5e47a937d7
adam-szczombrowski <adam.szczombrowski@gmail.com>
2016-09-23 14:25:05 +0200

    change badge for stars in index

--- app/react/components/RestaurantListItem.js
+++ app/react/components/RestaurantListItem.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import RatingStars from './RatingStars'
 
 const RestaurantsListItem = ({ restaurant }) => (
   <li className="list-group-item">
     <a href={`/restaurants/${restaurant.id}`}>{restaurant.name}</a>
-    <span className="badge pull-right">{restaurant.average_rating}</span>
-    <span className="badge pull-right primary">{restaurant.description}</span>
+    <div className="pull-right">
+      <span className="badge primary">{restaurant.description}</span>
+      <RatingStars rating={restaurant.average_rating} />
+    </div>
   </li>
 );
 
