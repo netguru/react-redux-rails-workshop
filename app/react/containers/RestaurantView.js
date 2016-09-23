ec61a9e7ee5f82d8011fbbd2d9d6ee2f4f2a9306
Paweł Jędrzejczyk <pawel.jedrzejczyk@netguru.pl>
2016-09-23 15:12:28 +0200

    comments view fix

--- app/react/containers/RestaurantView.js
+++ app/react/containers/RestaurantView.js
@@ -3,10 +3,12 @@ import RestaurantCommentList from 'components/RestaurantCommentList';
 import { connect } from 'react-redux';
 
 const RestaurantView = ({ restaurant }) => (
-  <div className="panel panel-primary">
-    <p className="panel-heading">{restaurant.name}</p>
-    <p className="panel-body">{restaurant.address}</p>
-    <p className="panel-body">{restaurant.description}</p>
+  <div>
+    <div className="panel panel-primary">
+      <p className="panel-heading">{restaurant.name}</p>
+      <p className="panel-body">{restaurant.address}</p>
+      <p className="panel-body">{restaurant.description}</p>
+    </div>
     <h2>Comments</h2>
     <RestaurantCommentList comments={restaurant.comments} />
   </div>
